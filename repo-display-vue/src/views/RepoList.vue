d0388597adb1d6e2eeb19e165ac77b760bcb1101
chuma-beep <wisboynelson123@gmail.com>
2024-06-05 13:01:12 +0100

    updating skeleton

--- repo-display-vue/src/views/RepoList.vue
+++ repo-display-vue/src/views/RepoList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div>
     <div class="w-full flex justify-center mb-10">
       <fwb-pagination
         v-model="currentPage"
@@ -7,12 +7,30 @@
         @page-changed="handlePageChange"
       ></fwb-pagination>
     </div>
-    <div class="flex flex-wrap justify-center mb-32">
+
+    <!--skeleton -->
+    <div v-if="loading" class="flex flex-wrap justify-center mb-10">
+      <div
+        v-for="index in perPage"
+        :key="index"
+        class="m-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4"
+      >
+        <div class="animate-pulse bg-gray-200 dark:bg-gray-800 rounded-md">
+          <h5 class="mb-2 h-6 bg-gray-300 dark:bg-gray-700 rounded"></h5>
+          <p class="h-4 bg-gray-300 dark:bg-gray-700 rounded"></p>
+          <p class="h-4 mb-1 bg-gray-300 dark:bg-gray-700 rounded"></p>
+          <p class="h-4 m-1 bg-gray-300 dark:bg-gray-700 rounded"></p>
+          <p class="h-4 mb-1 bg-gray-300 dark:bg-gray-700 rounded"></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-wrap justify-center">
       <fwb-card
         v-for="repo in paginatedData"
         :key="repo.id"
         href="#"
-        class="m-4 w-full md:w-1/3 lg:w-1/4"
+        class="m-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4"
       >
         <div class="p-5">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -35,9 +53,9 @@
           </a>
         </div>
       </fwb-card>
-      <p v-if="error" class="text-red-500 w-full text-center">{{ error }}</p>
-      <div v-if="loading" class="text-center text-gray-500 w-full">Loading...</div>
     </div>
+
+    <p v-if="error" class="text-red-500 w-full text-center">{{ error }}</p>
   </div>
 </template>
 
@@ -111,3 +129,20 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+@keyframes skeleton-pulse {
+  0% {
+    background-color: rgba(209, 213, 219, 0.5);
+  }
+  50% {
+    background-color: rgba(209, 213, 219, 1);
+  }
+  100% {
+    background-color: rgba(209, 213, 219, 0.5);
+  }
+}
+.animate-pulse {
+  animation: skeleton-pulse 1.5s infinite;
+}
+</style>
